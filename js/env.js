128c75f03f92566b8cdfab109575b26f78f94f85
RamyaPot <106664331+pottabathini99@users.noreply.github.com>
2024-06-02 22:08:54 +0530

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
-const contractAddress = ""; // Paste the address of the NFT collection.
+const contractAddress = "0x5F0037b6D9D90F6d86A7EeE1AcC8A7571c96fb41"; // Paste the address of the NFT collection.
 // uncomment the chain you are using:
 const chainId = "0x507"; // Moonbase
 // const chainId = "0x504"; // Moonbeam
